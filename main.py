ec5de7e0e23ebd6ce0729993c198bf55676e9779
AliceQiao <alice.qiao@ocsbstudent.ca>
2021-03-29 23:52:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+PI = 3.14
+diameter = float(input(' Please Enter the diameter of a circle: '))
+radius = diameter / 2
+area = PI * radius * radius
+circumference = 2 * PI * radius
+
+print(" Area Of a Circle = %.2f" %area)
+print(" Circumference Of a Circle = %.2f" %circumference)
